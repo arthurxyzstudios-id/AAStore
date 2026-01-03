6814ce8d7a5aa26e3023e60da1af7985e645c59e
arthurxyzstudios-id <bharaka.dzikri24@smk.belajar.id>
2026-01-03 08:12:38 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,5 @@
 import './globals.css';
 
-export const metadata = {
-  title: 'Panel Store',
-  description: 'Panel Pterodactyl Store',
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
